4e5dd9f0d0f02dfd748de69a2fc8190387e25956
Nerovia <nerovia@icloud.com>
2020-08-09 01:35:04 +0200

    Extentions

--- ConsoleArcade/Extentions.cs
+++ ConsoleArcade/Extentions.cs
@@ -2,9 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace ConsoleArcade.Sounds
+namespace ConsoleArcade
 {
-    class Extentions
+    static class Extentions
     {
+        static Random random;
+
+        static T PickRandom<T>(this List<T> list)
+        {
+            return list[random.Next(0, list.Count - 1)];
+        }
     }
 }
